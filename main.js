b0a462bac9c4f600114b2c2cf8b1dfd5164bb040
Nico Villaraza Ativo <nico.ativo@bicol-u.edu.ph>
2020-05-09 17:48:12 +0800

    Practical Components Modal
    *learned about communicaton between main Vue instance and Vue component through $emit events

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
+Vue.component('modal',{
+
+    template: `
+
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+                <div class="modal-card">
+                    <header class="modal-card-head">
+                    <p class="modal-card-title">Modal title</p>
+                    <button class="delete" aria-label="close" @click="$emit('close-modal')"></button>
+                    </header>
+                    <section class="modal-card-body">
+                        <slot></slot>
+                    </section>
+                    <footer class="modal-card-foot">
+                    <button class="button is-success">Save changes</button>
+                    <button class="button" @click="$emit('close-modal')">Cancel</button>
+                    </footer>
+                </div>
+        </div>
+    
+    
+    `
+
+
+});
+
 new Vue({
 
     el: "#root",
     data:{
-        message: "sample message"
+        message: "sample message",
+        showModal: false
     }
+
+
 
 });
