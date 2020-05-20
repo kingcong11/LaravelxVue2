f33246164b78002370573fba0a9e8891e6605cd4
Nico Villaraza Ativo <nico.ativo@bicol-u.edu.ph>
2020-05-20 19:17:19 +0800

    Named Slots
    *Learned about named slots and default content for a slot

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
+Vue.component('modal', {
+
+    template: `
+
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+            <p class="modal-card-title"><slot name="title">Default title</slot></p>
+            <button class="delete" aria-label="close" @click="$emit('close')"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot>Default Content</slot>
+            </section>
+            <footer class="modal-card-foot">
+                
+                <slot name="footer">
+                    <button class="button is-success" @click="$emit('close')">Okay</button>
+                </slot>
+            </footer>
+        </div>
+    </div>
+
+    `,
+});
+
 new Vue({
 
     el: "#root",
     data:{
-        message: "sample message"
+        message: "sample message",
+        showModalOne: false
+    },
+
+
+    methods: {
+        closeModal(modalName){
+            this[modalName] = false;
+        }
     }
 
 });
