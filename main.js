3058aa1d542dde1f571c91fabeb4010a7815dd51
Nico Villaraza Ativo <nico.ativo@bicol-u.edu.ph>
2020-05-20 17:40:35 +0800

    Custom Event
    *Learned creating custom events

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+Vue.component('coupon', {
+    template: `
+        <input type="text" placeholder="Enter Coupon Code" @blur="onCouponApplied"></input>
+    `,
+
+    methods: {
+        onCouponApplied(){
+            this.$emit('applied');
+        }
+    }
+});
+
 new Vue({
 
     el: "#root",
     data:{
-        message: "sample message"
-    }
+        message: "sample message",
+        couponApplied : false
+    },
+
+    methods: {
+        onCouponApplied(){
+            // alert(`Coupon Applied!! Balagaboom`);
+            this.couponApplied = true;
+        }
+    },
 
 });
